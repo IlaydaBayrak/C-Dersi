9efa4bbf28e9de69662b16cb26c7db534cb53635
İlayda BAYRAK <148214917+IlaydaBayrak@users.noreply.github.com>
2024-05-27 21:58:53 +0300

    Daire Alanı
    lesson-9

---
+++ 09-Dairenin alanı.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	double r, alan = 0, cevre = 0;
+	double pi = 3.14;
+	r=4;
+	
+	alan = pi*r*r;
+	cevre = 2*pi*r;
+	
+	printf("alan = %g cevre = %g", alan, cevre);
+	
+return 0;
+}

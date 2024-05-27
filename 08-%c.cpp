ad9c83773aae0316e1e8554b6809f10e61635ea3
İlayda BAYRAK <148214917+IlaydaBayrak@users.noreply.github.com>
2024-05-27 21:58:29 +0300

    Char
    lesson-8

---
+++ 08-%c.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+char a = '2';		//int yerine char kullanmak bellekte daha az yer kaplayacaktýr 0:255 arasýnda char veri tipi kullanýlabilir.
+
+printf("a = %c",a);
+
+return 0;
+}
+

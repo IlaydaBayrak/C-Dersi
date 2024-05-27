cb0304ae54d8a158371f6486f1eee56bf3ce9e21
İlayda BAYRAK <148214917+IlaydaBayrak@users.noreply.github.com>
2024-05-27 20:34:54 +0300

    Temel Kavramlar
    lesson-1

---
+++ 01-Temel kavramlar.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>  /*stdio.h adlý baþlýk dosyasýný çaðýrýr. C dilinin standart kütüphane dosyalarýndan biridir.*/
+					//Bu dosya C dilinde giriþ/çýkýþ iþlemleri yapan fonksiyonlarý içerir.//
+
+int main(void)   //return 0; yerine void yazýlabilir.
+{
+	/*açýklama tek satýra sýðmýyorsa bu simgelerin arasýna yazýlýr*/
+	//açýklama tek satýra sýðýyor ise bu kullanýlýr//
+	
+	printf("hello \t");	
+
+	{
+
+	printf("world");
+	return 0;
+	}
+}

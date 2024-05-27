f1a44e7d165aa5762d5a0c632abc94152fdcae7f
İlayda BAYRAK <148214917+IlaydaBayrak@users.noreply.github.com>
2024-05-27 21:59:28 +0300

    Ters slash
    lesson-10

---
+++ 10-Ters slash.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+
+{
+	printf("ilayda \nbayrak \n\n");	//alt satıra geç
+	printf("ilayda \tbayrak \n\n");	//tab
+	printf("\'ilayda\'\n\n");		//tek tırnak
+	printf("\"bayrak\"\n\n");		//çift tırnak
+	printf("ilayda\\ bayrak \n\n");	//ters bölü
+	printf("ilayda\?");				//soru işareti
+	
+	return 0;
+}

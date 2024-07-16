1135b54e4f44835cc360822603960b7b449151c9
İlayda BAYRAK <148214917+IlaydaBayrak@users.noreply.github.com>
2024-07-16 13:29:22 +0300

    puts
    lesson-53

---
+++ 53-puts.cpp
@@ -0,0 +1,13 @@
+//PUTS
+#include<stdio.h>
+
+int main(){
+	
+	char ad[]="ilayda";
+	char soyad[]="BAYRAK";
+	
+	puts(ad);		//puts(dizi_adý)
+	puts(soyad);	//\n ifadesine gerek yoktur. Kendisi otomatik olarak alt satýra geçer.
+	
+	return 0;
+}
